099c9f91d9b69c7e0d7a4e47419c227bb2ed6014
devavology <145644105+devavology@users.noreply.github.com>
2025-07-17 16:00:30 +0530

    first commit

---
+++ V1-(Email+Password)/css/js/custom.js
@@ -0,0 +1,47 @@
+                // js==1
+
+$(".pic-deliv a").click(function(){
+    $(this).addClass("active").siblings().removeClass("active")
+    });
+
+
+                  // js==2
+
+$(".list_tip li").click(function(){
+$(this).addClass("active").siblings().removeClass("active")
+});
+
+
+
+                      // js==3
+
+$(".stp-btn a").click(function(){
+    $(this).addClass("active").siblings().removeClass("active")
+    });
+
+
+                    // js==4
+
+ $(".price-range span").click(function(){
+        $(this).addClass("select-price").siblings().removeClass("select-price")
+        });
+    
+
+
+
+                            // js==5
+
+        $(".fav-icons").click(function() {
+            var svgHeart = $(this).find('.svg_heart');
+            var heartIcon = $(this).find('.heart_icon');
+            
+            if (svgHeart.css("display") === "none") {
+                svgHeart.css("display", "block");
+                heartIcon.css("display", "none");
+            } else {
+                svgHeart.css("display", "none");
+                heartIcon.css("display", "block");
+            }
+        });
+        
+        
